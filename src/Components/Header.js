b96998510e89050d1343fdec65fa33b1f4f77d0b
Julie P <paupertjulie@gmail.com>
2020-09-06 14:18:30 +0200

    menu responsive

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -12,6 +12,7 @@ function Header() {
     switch (location.pathname) {
         case '/':
             header += " home";
+            btn += " btn-home"
             break;
         case "/shop":
             header += " shop";
@@ -25,10 +26,10 @@ function Header() {
         <header className={header}>
             <Menu />
             <div className="container spacing">
-            <h1 className="header__primaryTitle">Amazing Shoes at an Amazing Price</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa quam perspiciatis facilis beatae laudantium
-                quidem enim sit sequi!</p>
-            <Link to="/shop" className={btn}>See what we have</Link>
+                <h1 className="header__primaryTitle">Amazing shoes at an amazing price</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa quam perspiciatis facilis beatae laudantium
+                    quidem enim sit sequi!</p>
+                <Link to="/shop" className={btn}>See what we have</Link>
             </div>
         </header>
     )
