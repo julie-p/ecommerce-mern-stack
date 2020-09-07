6f9924a64acc211c488cc381ccc3cd6f54e6b50d
Julie P <paupertjulie@gmail.com>
2020-09-07 18:50:37 +0200

    added discount

--- src/data.js
+++ src/data.js
@@ -5,42 +5,48 @@ export default {
             price: 99,
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa quam perspiciatis facilis beatae laudantium quidem enim sit sequi!', 
             image: '../imgs/shoe-1.png',
-            featured: false
+            featured: false,
+            discount: 0
         },
         {
             name: 'Shoe 2', 
             price: 115,
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa quam perspiciatis facilis beatae laudantium quidem enim sit sequi!', 
             image: '../imgs/shoe-2.png',
-            featured: false
+            featured: false,
+            discount: 0
         },
         {
             name: 'Shoe 3', 
             price: 120,
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa quam perspiciatis facilis beatae laudantium quidem enim sit sequi!', 
             image: '../imgs/shoe-3.png',
-            featured: false
+            featured: false,
+            discount: 0
         },
         {
             name: 'Shoe 4', 
             price: 99,
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa quam perspiciatis facilis beatae laudantium quidem enim sit sequi!', 
             image: '../imgs/shoe-4.png',
-            featured: true
+            featured: true,
+            discount: 20
         },
         {
             name: 'Shoe 5', 
             price: 110,
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa quam perspiciatis facilis beatae laudantium quidem enim sit sequi!', 
             image: '../imgs/shoe-5.png',
-            featured: true
+            featured: true,
+            discount: 20
         },
         {
             name: 'Shoe 6', 
             price: 130,
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa quam perspiciatis facilis beatae laudantium quidem enim sit sequi!', 
             image: '../imgs/shoe-6.png',
-            featured: true
+            featured: true,
+            discount: 20
         },
     ]
 };
