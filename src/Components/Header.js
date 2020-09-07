fb2dee2da0db0817a7663d0b9ca7e95642bdce11
Julie P <paupertjulie@gmail.com>
2020-09-07 13:05:50 +0200

    homepage styling done

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -11,14 +11,17 @@ function Header() {
 
     let header = "header__hero";
     let btn = "btn";
+    let h1;
     switch (location.pathname) {
         case '/':
             header += " home";
-            btn += " btn-home"
+            btn += " btn-home";
+            h1 = <h1 className="header__primaryTitle">Amazing shoes at an amazing price</h1>;
             break;
         case "/shop":
             header += " shop";
             btn += " btn-shop";
+            h1 = <h1 className="header__primaryTitle title__shop">Find your perfect shoes</h1>
             break;
         default :
             header = "header__hero";
@@ -33,7 +36,7 @@ function Header() {
                 </Link>
             </div>
             <div className="container spacing">
-                <h1 className="header__primaryTitle">Amazing shoes at an amazing price</h1>
+                {h1}
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa quam perspiciatis facilis beatae laudantium
                     quidem enim sit sequi!</p>
                 <Link to="/shop" className={btn}>See what we have</Link>
--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -1,10 +1,64 @@
 import React from 'react';
 import Header from '../Components/Header';
+import '../styles/Homepage.css';
+import { Link } from 'react-router-dom';
 
 function Homepage() {
     return (
         <div>
-           <Header /> 
+           <Header />
+
+            <main>
+                <section className="featured">
+                    <div className="container">
+                        <h2 className="featured__sectionTitle">Featured Products</h2>
+                    </div>  
+                    <div className="split">
+                    <Link className="featured__item">
+                        <img src="../imgs/shoe-4.png" alt="" className="featured__img" />
+                        <p className="featured__details"><span className="price">$99</span>shoe name</p>
+                    </Link>
+                    <Link className="featured__item">
+                        <img src="../imgs/shoe-5.png" alt="" className="featured__img" />
+                        <p className="featured__details"><span className="price">$99</span>shoe name</p>
+                    </Link>
+                    <Link className="featured__item">
+                        <img src="../imgs/shoe-6.png" alt="" className="featured__img" />
+                        <p className="featured__details"><span className="price">$99</span>shoe name</p>
+                    </Link>
+                    </div>
+                </section> 
+
+                <section className="products">
+                    <div className="container">
+                        <h2 className="products__sectionTitle">Our products</h2>
+
+                        <article className="product shoe-red spacing">
+                            <img src="../imgs/shoe-1.png" alt="" className="products__image"/>
+                            <h3 className="products__title">A really nice shoe</h3>
+                            <p className="products__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa quam
+                                perspiciatis facilis beatae laudantium quidem enim sit sequi!</p>
+                            <Link className="btn">Buy now</Link>
+                        </article>
+
+                        <article className="product shoe-white shoe-left spacing">
+                            <img src="../imgs/shoe-2.png" alt="" className="products__image"/>
+                            <h3 className="products__title">A really nice shoe</h3>
+                            <p className="products__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa quam
+                                perspiciatis facilis beatae laudantium quidem enim sit sequi!</p>
+                            <Link className="btn">Buy now</Link>
+                        </article>
+
+                        <article className="product shoe-blue spacing">
+                            <img src="../imgs/shoe-3.png" alt="" className="products__image"/>
+                            <h3 className="products__title">A really nice shoe</h3>
+                            <p className="products__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa quam
+                                perspiciatis facilis beatae laudantium quidem enim sit sequi!</p>
+                            <Link className="btn">Buy now</Link>
+                        </article>
+                    </div>
+                </section>
+            </main>
         </div>
     )
 };
