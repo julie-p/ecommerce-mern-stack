655d5ec4f49157179c96a39478ca00bc07e906ca
Julie P <paupertjulie@gmail.com>
2020-09-07 16:07:02 +0200

    change price

--- src/data.js
+++ src/data.js
@@ -9,14 +9,14 @@ export default {
         },
         {
             name: 'Shoe 2', 
-            price: 99,
+            price: 115,
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa quam perspiciatis facilis beatae laudantium quidem enim sit sequi!', 
             image: '../imgs/shoe-2.png',
             featured: false
         },
         {
             name: 'Shoe 3', 
-            price: 99,
+            price: 120,
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa quam perspiciatis facilis beatae laudantium quidem enim sit sequi!', 
             image: '../imgs/shoe-3.png',
             featured: false
@@ -30,14 +30,14 @@ export default {
         },
         {
             name: 'Shoe 5', 
-            price: 99,
+            price: 110,
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa quam perspiciatis facilis beatae laudantium quidem enim sit sequi!', 
             image: '../imgs/shoe-5.png',
             featured: true
         },
         {
             name: 'Shoe 6', 
-            price: 99,
+            price: 130,
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa quam perspiciatis facilis beatae laudantium quidem enim sit sequi!', 
             image: '../imgs/shoe-6.png',
             featured: true
