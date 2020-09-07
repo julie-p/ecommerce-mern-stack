c4f95932c5daae2d1c6cd7d32e65679d8821ba5a
Julie P <paupertjulie@gmail.com>
2020-09-07 13:23:28 +0200

    homepage dynamic data

--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -2,8 +2,44 @@ import React from 'react';
 import Header from '../Components/Header';
 import '../styles/Homepage.css';
 import { Link } from 'react-router-dom';
+import data from '../data';
 
 function Homepage() {
+
+    const featured = data.products.map((product, key) => {
+        if (product.featured) {
+            return <Link className="featured__item">
+                        <img src={product.image} alt={"shoe__" + key} className="featured__img" />
+                        <p className="featured__details"><span className="price">${product.price}</span>{product.name}</p>
+                   </Link>
+        }
+    });
+
+    const products = data.products.map((product, key) => {
+        if (!product.featured && product.name === 'Shoe 1') {
+            return <article className="product shoe-red spacing">
+                        <img src={product.image} alt={"shoe__" + key} className="products__image"/>
+                        <h3 className="products__title">A really nice shoe</h3>
+                        <p className="products__description">{product.description}</p>
+                        <Link className="btn">Buy now</Link>
+                   </article>
+        } else if (!product.featured && product.name === 'Shoe 2') {
+            return <article className="product shoe-white shoe-left spacing">
+                        <img src={product.image} alt={"shoe__" + key} className="products__image"/>
+                        <h3 className="products__title">A really nice shoe</h3>
+                        <p className="products__description">{product.description}</p>
+                        <Link className="btn">Buy now</Link>
+                   </article>
+        } else if (!product.featured && product.name === 'Shoe 3') {
+            return <article className="product shoe-blue spacing">
+                        <img src={product.image} alt={"shoe__" + key} className="products__image"/>
+                        <h3 className="products__title">A really nice shoe</h3>
+                        <p className="products__description">{product.description}</p>
+                        <Link className="btn">Buy now</Link>
+                   </article>
+        }
+    })
+
     return (
         <div>
            <Header />
@@ -14,48 +50,14 @@ function Homepage() {
                         <h2 className="featured__sectionTitle">Featured Products</h2>
                     </div>  
                     <div className="split">
-                    <Link className="featured__item">
-                        <img src="../imgs/shoe-4.png" alt="" className="featured__img" />
-                        <p className="featured__details"><span className="price">$99</span>shoe name</p>
-                    </Link>
-                    <Link className="featured__item">
-                        <img src="../imgs/shoe-5.png" alt="" className="featured__img" />
-                        <p className="featured__details"><span className="price">$99</span>shoe name</p>
-                    </Link>
-                    <Link className="featured__item">
-                        <img src="../imgs/shoe-6.png" alt="" className="featured__img" />
-                        <p className="featured__details"><span className="price">$99</span>shoe name</p>
-                    </Link>
+                        {featured}
                     </div>
                 </section> 
 
                 <section className="products">
                     <div className="container">
                         <h2 className="products__sectionTitle">Our products</h2>
-
-                        <article className="product shoe-red spacing">
-                            <img src="../imgs/shoe-1.png" alt="" className="products__image"/>
-                            <h3 className="products__title">A really nice shoe</h3>
-                            <p className="products__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa quam
-                                perspiciatis facilis beatae laudantium quidem enim sit sequi!</p>
-                            <Link className="btn">Buy now</Link>
-                        </article>
-
-                        <article className="product shoe-white shoe-left spacing">
-                            <img src="../imgs/shoe-2.png" alt="" className="products__image"/>
-                            <h3 className="products__title">A really nice shoe</h3>
-                            <p className="products__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa quam
-                                perspiciatis facilis beatae laudantium quidem enim sit sequi!</p>
-                            <Link className="btn">Buy now</Link>
-                        </article>
-
-                        <article className="product shoe-blue spacing">
-                            <img src="../imgs/shoe-3.png" alt="" className="products__image"/>
-                            <h3 className="products__title">A really nice shoe</h3>
-                            <p className="products__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa quam
-                                perspiciatis facilis beatae laudantium quidem enim sit sequi!</p>
-                            <Link className="btn">Buy now</Link>
-                        </article>
+                        {products}
                     </div>
                 </section>
             </main>
---
+++ src/data.js
@@ -0,0 +1,46 @@
+export default {
+    products: [
+        {
+            name: 'Shoe 1', 
+            price: 99,
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa quam perspiciatis facilis beatae laudantium quidem enim sit sequi!', 
+            image: '../imgs/shoe-1.png',
+            featured: false
+        },
+        {
+            name: 'Shoe 2', 
+            price: 99,
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa quam perspiciatis facilis beatae laudantium quidem enim sit sequi!', 
+            image: '../imgs/shoe-2.png',
+            featured: false
+        },
+        {
+            name: 'Shoe 3', 
+            price: 99,
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa quam perspiciatis facilis beatae laudantium quidem enim sit sequi!', 
+            image: '../imgs/shoe-3.png',
+            featured: false
+        },
+        {
+            name: 'Shoe 4', 
+            price: 99,
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa quam perspiciatis facilis beatae laudantium quidem enim sit sequi!', 
+            image: '../imgs/shoe-4.png',
+            featured: true
+        },
+        {
+            name: 'Shoe 5', 
+            price: 99,
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa quam perspiciatis facilis beatae laudantium quidem enim sit sequi!', 
+            image: '../imgs/shoe-5.png',
+            featured: true
+        },
+        {
+            name: 'Shoe 6', 
+            price: 99,
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa quam perspiciatis facilis beatae laudantium quidem enim sit sequi!', 
+            image: '../imgs/shoe-6.png',
+            featured: true
+        },
+    ]
+};
