730644f3f944c70bb1cb8d60d31b60d26d20738e
Julie P <paupertjulie@gmail.com>
2020-09-28 12:22:24 +0200

    test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Homepage from './screens/Homepage';
-import Shop from './screens/Shop';
-import Outlet from './screens/Outlet';
-import ProductDetails from './screens/ProductDetails';
-import Cart from './screens/Cart';
+import Homepage from './Screens/Homepage';
+import Shop from './Screens/Shop';
+import Outlet from './Screens/Outlet';
+import ProductDetails from './Screens/ProductDetails';
+import Cart from './Screens/Cart';
 
 function App() {
   return (
