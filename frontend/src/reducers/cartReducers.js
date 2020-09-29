81493b1791ae25157adcc73f50375b4c33a1fec2
Julie P <paupertjulie@gmail.com>
2020-09-29 18:04:43 +0200

    refactoring

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -11,11 +11,13 @@ function cartReducer(state= { cartItems:[] }, action) {
                         state.cartItems.map(x => x.product === product.product ? item : x)
                 }
             }
-            return { cartItems: [...state.cartItems, item] };
+            return { 
+                cartItems: [...state.cartItems, item] 
+            };
         case CART_REMOVE_ITEM : 
             return { 
                 cartItems: state.cartItems.filter(x => x.product !== action.payload)
-            }
+            };
         default : 
             return state;
     } 
