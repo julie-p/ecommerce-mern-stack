386bcf895c9e188f06b8761011ea8fc911176a48
Julie P <paupertjulie@gmail.com>
2020-12-21 18:44:46 +0100

    build for hosting

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -7,8 +7,8 @@ const router = express.Router();
 router.get('/createadmin', async (req, res) => {
     try {
         const user = new User({ 
-            email: '****@gmail.com',
-            password: '****',
+            email: 'paupertjulie@gmail.com',
+            password: '1234',
             isAdmin: true
         });
     
